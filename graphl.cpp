3465b9dbdb5395a80d5df22f5afbae1061433bfd
bryanadamt <bryanat@uw.edu>
2021-05-15 12:19:49 -0700

    right so everything is ok for the given thing but uwb still wrong

--- graphl.cpp
+++ graphl.cpp
@@ -119,17 +119,3 @@ void GraphL::dfsHelper(int source) {
         traverse = traverse->nextEdge;
     }
 }
-
-int main() {
-    ifstream infile2("data.txt");
-    //for each graph, find the depth-first search ordering
-	for (;;) {
-		GraphL G;
-		G.buildGraph(infile2);
-	    if (infile2.eof())
-		break;
-		// G.displayGraph();
-		G.depthFirstSearch();    // find and display depth-first ordering to cout
-	}
-    return 0;
-}
